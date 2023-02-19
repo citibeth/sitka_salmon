ddc43e664807e8995f72b83f8ed4f1f38ddedb79
eafischer2 <eafischer2@alaska.edu>
2023-02-19 12:12:41 -0900

    Adding Google Colab stuff;

---
+++ push_to_google_drive.sh
@@ -0,0 +1,3 @@
+# Copy data from local hard drive to Google Drive where it can be used from Google Colab
+rclone copy --drive-root-folder-id=1-C6OpKVM73g2lCEpaoWZkB_-Y8-xKwfM ../output/ alaska:/
+rclone copy --drive-root-folder-id=11gO6RZqpkbQSqLbXqzzqK9-h6ZOAnWZ5 ../data/ alaska:/
--- sitka_salmon/config.py
+++ sitka_salmon/config.py
@@ -1,4 +1,11 @@
 import os
 
-HARNESS = os.path.abspath(os.path.os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-
+# Locate data files
+try:
+    # Running on Google Colab?  Use files from Google Drive
+    import google.colab
+    google.colab.drive.mount("/content/drive")
+    HARNESS = '/content/drive/MyDrive/sitka_salmon'
+except ModuleNotFoundError:
+    # Running locally, key off of location of source in the filesystem
+    HARNESS = os.path.abspath(os.path.os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
