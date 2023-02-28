001d690643365f0a30942eea27b3c8cc9cff5fd3
eafischer2 <eafischer2@alaska.edu>
2023-02-27 19:56:08 -0900

    Bug fix

--- sitka_salmon/config.py
+++ sitka_salmon/config.py
@@ -5,7 +5,7 @@ try:
     # Running on Google Colab?  Use files from Google Drive
     import google.colab
     google.colab.drive.mount("/content/drive")
-    HARNESS = '/content/fiudrive/MyDrive/sk'
+    HARNESS = '/content/drive/MyDrive/sk'
 except ModuleNotFoundError:
     # Running locally, key off of location of source in the filesystem
     HARNESS = os.path.abspath(os.path.os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
